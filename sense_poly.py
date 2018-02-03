c0f47c912866473445758e3a6756f1077c1b359d
automationgeek <jf.tremblay@gmail.com>
2018-02-03 10:21:59 -0500

    Update sense_poly.py

--- sense_poly.py
+++ sense_poly.py
@@ -26,7 +26,6 @@ class Controller(polyinterface.Controller):
     def __init__(self, polyglot):
         super(Controller, self).__init__(polyglot)
         self.name = 'Sense'
-        self.initialized = False
         self.tries = 0
         self.email = None
         self.password = None
@@ -36,7 +35,6 @@ class Controller(polyinterface.Controller):
         LOGGER.info('Started Sense NodeServer version %s', str(VERSION))
         try:
             
-            self.setDriver('ST', 0)
             if 'email' in self.polyConfig['customParams'] and self.email is None:
                 self.email = self.polyConfig['customParams']['email']
                 LOGGER.info('Custom Email address specified: {}'.format(self.email))
@@ -60,46 +58,38 @@ class Controller(polyinterface.Controller):
         
         
     def shortPoll(self):
-        self.query()
+        self.connectSense()
+        for node in self.nodes:
+            self.nodes[node].query()
 
     def longPoll(self):
         self.discover()
     
-    def __connect(self):
+    def connectSense(self):
         try:
-            # Force a reconnect need to find a better solutions
             self.sense =  Senseable(self.email,self.password)
         except Exception as ex:
             LOGGER.error('Unable to connect to Sense API: %s', str(ex))
     
     def query(self):
-        
-        self.__connect()
         try:
-            self.setDriver('ST', 1)
-            self.setDriver('CPW', int(self.sense.active_power))
-            self.setDriver('GV6', int(self.sense.active_solar_power))
-            self.setDriver('GV7', int(self.sense.daily_usage))
-            self.setDriver('GV8', int(self.sense.daily_production))
-            self.setDriver('GV9', int(self.sense.weekly_usage))
-            self.setDriver('GV10', int(self.sense.weekly_production))
-            self.setDriver('GV11', int(self.sense.monthly_usage))
-            self.setDriver('GV12', int(self.sense.monthly_production))
-            self.setDriver('GV13', int(self.sense.yearly_usage))
-            self.setDriver('GV14', int(self.sense.yeary_production))
+            self.setDriver('ST', 1, True)
+            self.setDriver('CPW', int(self.sense.active_power), True)
+            self.setDriver('GV6', int(self.sense.active_solar_power), True)
+            self.setDriver('GV7', int(self.sense.daily_usage), True)
+            self.setDriver('GV8', int(self.sense.daily_production), True)
+            self.setDriver('GV9', int(self.sense.weekly_usage), True)
+            self.setDriver('GV10', int(self.sense.weekly_production), True)
+            self.setDriver('GV11', int(self.sense.monthly_usage), True)
+            self.setDriver('GV12', int(self.sense.monthly_production), True)
+            self.setDriver('GV13', int(self.sense.yearly_usage), True)
+            self.setDriver('GV14', int(self.sense.yeary_production), True)
         except Exception as ex:
             LOGGER.error('query, unable to retrieve Sense Monitor usage: %s', str(ex))
-
-        for node in self.nodes:
-            if self.nodes[node].address != self.address and self.nodes[node].do_poll:
-                self.nodes[node].query()
-        self.reportDrivers()
-        # del self.sense
         
     def discover(self, *args, **kwargs):
-        self.__connect()
+        time.sleep(1)
         try :
-            time.sleep(1)
             for device in  self.sense.get_discovered_device_data():
                 if device is not None: 
                     if device['tags']['Revoked'] == 'false':
@@ -107,10 +97,9 @@ class Controller(polyinterface.Controller):
         except Exception as ex:
             LOGGER.error('discover: %s', str(ex))
     
-        # del self.sense
-    
     def delete(self):
         LOGGER.info('Deleting Sense Node Server')
+        
     id = 'controller'
     commands = {
                     'QUERY': query          
@@ -130,42 +119,36 @@ class Controller(polyinterface.Controller):
 class SenseDetectedDevice(polyinterface.Node):
 
     def __init__(self, controller, primary, address, name):
+        super(SenseDetectedDevice, self).__init__(controller, primary, address.lower(), name)
         self.nameOrig = name
         self.addressOrig = address
-        super(SenseDetectedDevice, self).__init__(controller, primary, address.lower(), name)
-        self.do_poll = True
         self.timeout = 5.0
-        self.query()
-        
+          
     def start(self):
-        pass                                     
-    
+        self.query()                                   
+        
     def query(self):
-        self.updateDevice()
-        # self.reportDrivers()
-
-    def updateDevice(self):
         try :
         
             # Device Power Status
-            self.setDriver('ST', 0)
+            self.setDriver('ST', 0,True)
             for x in self.parent.sense.active_devices:
                 if x == self.nameOrig:
-                    self.setDriver('ST', 100)
+                    self.setDriver('ST', 100, True)
 
             # Device Info
             deviceInfo = self.parent.sense.get_device_info(self.addressOrig)
             if deviceInfo is not None:
                     if 'usage' in deviceInfo : 
-                        self.setDriver('GV1', int(deviceInfo['usage']['avg_monthly_runs']))
-                        self.setDriver('GV5', int(deviceInfo['usage']['avg_watts']))
-                        self.setDriver('GV2', int(deviceInfo['usage']['avg_monthly_KWH']))
-                        self.setDriver('GV3', int(deviceInfo['usage']['current_month_runs']))
-                        self.setDriver('GV4', int(deviceInfo['usage']['current_month_KWH']))
+                        self.setDriver('GV1', int(deviceInfo['usage']['avg_monthly_runs']),True)
+                        self.setDriver('GV5', int(deviceInfo['usage']['avg_watts']),True)
+                        self.setDriver('GV2', int(deviceInfo['usage']['avg_monthly_KWH']),True)
+                        self.setDriver('GV3', int(deviceInfo['usage']['current_month_runs']),True)
+                        self.setDriver('GV4', int(deviceInfo['usage']['current_month_KWH']),True)
                         
         except Exception as ex:
             LOGGER.error('updateDevice: %s', str(ex))
-        
+
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 78},
                {'driver': 'GV5', 'value': 0, 'uom': 73}, 
                {'driver': 'GV1', 'value': 0, 'uom': 0}, 
