87500662b7018562e9f53e72cde362187495b73a
automationgeek <jf.tremblay@gmail.com>
2021-01-23 15:37:49 -0500

    Update sense_poly.py

--- sense_poly.py
+++ sense_poly.py
@@ -105,7 +105,7 @@ class Controller(polyinterface.Controller):
         except Exception as ex:
             LOGGER.error('query, unable to retrieve Sense Monitor usage: %s', str(ex))
         
-        #self.reportDrivers()
+        self.reportDriver()
         
         for node in self.nodes:
             if  self.nodes[node].queryON == True :
@@ -229,7 +229,7 @@ class SenseDetectedDevice(polyinterface.Node):
         except Exception as ex:
             LOGGER.error('updateDevice: %s', str(ex))
 
-        #self.reportDrivers()
+        self.reportDriver()
             
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 78},
                {'driver': 'GV5', 'value': 0, 'uom': 73}, 
